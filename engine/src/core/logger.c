3862b30fd1f113698c0bdc70b82ce2a728f9fc20
cykadash <infves@gmail.com>
2022-02-17 21:48:17 -0500

    vscode

--- engine/src/core/logger.c
+++ engine/src/core/logger.c
@@ -20,15 +20,12 @@ void log_output(log_level level, const char* message, ...) {
     b8 is_error = level < LOG_LEVEL_WARN;
 
     // Technically imposes a 32k character limit on a single log entry, but...
-    // DON'T DO THAT!
+    // Don't do that
     const i32 msg_length = 32000;
     char out_message[msg_length];
     memset(out_message, 0, sizeof(out_message));
 
     // Format original message.
-    // NOTE: Oddly enough, MS's headers override the GCC/Clang va_list type with a "typedef char* va_list" in some
-    // cases, and as a result throws a strange error here. The workaround for now is to just use __builtin_va_list,
-    // which is the type GCC/Clang's va_start expects.
     __builtin_va_list arg_ptr;
     va_start(arg_ptr, message);
     vsnprintf(out_message, msg_length, message, arg_ptr);
